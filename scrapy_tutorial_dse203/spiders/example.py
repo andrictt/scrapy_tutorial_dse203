6ca630f0fd524359c33553a916dac23d09f28475
Beidan Huang <beidan.huang@rady.ucsd.edu>
2020-10-20 08:52:02 -0700

    first commit

---
+++ scrapy_tutorial_dse203/spiders/example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+
+
+    def start_requests(self):
+        start_urls = ['https://en.wikipedia.org/wiki/Apple_Inc.']
+
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
